e65de4b7d9af968be725a7bd3b61189a43d2c186
Jose <magicomiralles@gmail.com>
2017-12-02 17:02:05 -0500

    Added TAC website.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+TODO: remove or relpace all instances of "php" with "html".
+
 ## Welcome to GitHub Pages
 
 You can use the [editor on GitHub](https://github.com/JoseMiralles/JoseMiralles.github.io/edit/master/README.md) to maintain and preview the content for your website in Markdown files.
---
+++ TAC/applyasafighter.php
@@ -0,0 +1,386 @@
+<html>
+    <link rel="SHORTCUT ICON" href="favicon.png">
+    
+    
+    <body>
+        
+
+    
+
+<?php include 'Header.php'; ?>
+
+        <div id="container">
+            
+            
+            
+            <div id="sitecontent">
+                
+                <div id="site" class="relative">
+                    <div id="guts">
+                        <div id="content">
+                            <div id="newsPage">
+
+                                <div class="pageheader">
+                                    <div class="heading wow fadeInLeft" style="margin-bottom: 8px;">FIGHTER FORM</div> <!--Title------------------------>
+                                    <div class="note wow fadeInUp" data-wow-delay="1s">Fight on one of our upcoming events!<img class="lefttriangle" src="img/site-graphics/icons/leftcorner.png"></div>
+                                </div>
+
+                                <div class="left leftside">
+                                    
+                                    <!--<div class="contentbox">
+                                        <div class="redheader">What Can We Offer?</div>
+                                        <p> "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." </p>
+                                    </div>-->
+
+                                    <div class="contentbox">
+                                        <div id="leftitem">
+                                            <div class="redheader" style="margin-bottom: 8px">Application</div>
+                                            
+                                            <!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:none; clear:left; font:14px Helvetica,Arial,sans-serif;  width:100%;}                                      
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="http://victoriummma.us3.list-manage.com/subscribe/post?u=9fee9de85873d2979b7968c45&amp;id=d2fee3952d" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+	
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-FNAME">Fist Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-LNAME">Last Name  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+</div>
+<div class="mc-field-group size1of2">
+	<label for="mce-PHONE">Phone Number  <span class="asterisk">*</span>
+</label>
+    <div class="phonefield phonefield-us">
+        (<span class="phonearea"><input pattern="[0-9]*" id="mce-PHONE-area" name="PHONE[area]" maxlength="3" size="3" value="" type="text"></span>)
+        <span class="phonedetail1"><input pattern="[0-9]*" id="mce-PHONE-detail1" name="PHONE[detail1]" maxlength="3" size="3" value="" type="text"></span> - 
+        <span class="phonedetail2"><input pattern="[0-9]*" id="mce-PHONE-detail2" name="PHONE[detail2]" maxlength="4" size="4" value="" type="text"></span>
+		<span class="small-meta nowrap">(###) ###-####</span>
+		<div class="fake-phone"><input type="hidden" class="required phone" id="PHONE-fake-phone" value=""></div>
+    </div>
+</div><div class="mc-field-group">
+	<label for="mce-STATE">State of Residence  <span class="asterisk">*</span>
+</label>
+	<select name="STATE" class="required" id="mce-STATE">
+	<option value=""></option>
+	<option value="Alabama">Alabama</option>
+<option value="Alaska">Alaska</option>
+<option value="Arizona">Arizona</option>
+<option value="Arkansas">Arkansas</option>
+<option value="California">California</option>
+<option value="Colorado">Colorado</option>
+<option value="Connecticut">Connecticut</option>
+<option value="Delaware">Delaware</option>
+<option value="Florida">Florida</option>
+<option value="Georgia">Georgia</option>
+<option value="Hawaii">Hawaii</option>
+<option value="Idaho">Idaho</option>
+<option value="Illinois">Illinois</option>
+<option value="Indiana">Indiana</option>
+<option value="Iowa">Iowa</option>
+<option value="Kansas">Kansas</option>
+<option value="Kentucky">Kentucky</option>
+<option value="Louisiana">Louisiana</option>
+<option value="Maine">Maine</option>
+<option value="Maryland">Maryland</option>
+<option value="Massachusetts">Massachusetts</option>
+<option value="Michigan">Michigan</option>
+<option value="Minnesota">Minnesota</option>
+<option value="Mississippi">Mississippi</option>
+<option value="Missouri">Missouri</option>
+<option value="Montana">Montana</option>
+<option value="Nebraska">Nebraska</option>
+<option value="Nevada">Nevada</option>
+<option value="New Hampshire">New Hampshire</option>
+<option value="New Jersey">New Jersey</option>
+<option value="New Mexico">New Mexico</option>
+<option value="New York">New York</option>
+<option value="North Carolina">North Carolina</option>
+<option value="North Dakota">North Dakota</option>
+<option value="Ohio">Ohio</option>
+<option value="Oklahoma">Oklahoma</option>
+<option value="Oregon">Oregon</option>
+<option value="Pennsylvania">Pennsylvania</option>
+<option value="Rhode Island">Rhode Island</option>
+<option value="South Carolina">South Carolina</option>
+<option value="South Dakota">South Dakota</option>
+<option value="Tennessee">Tennessee</option>
+<option value="Texas">Texas</option>
+<option value="Utah">Utah</option>
+<option value="Vermont">Vermont</option>
+<option value="Virginia">Virginia</option>
+<option value="Washington">Washington</option>
+<option value="Washington D.C.">Washington D.C.</option>
+<option value="West Virginia">West Virginia</option>
+<option value="Wisconsin">Wisconsin</option>
+<option value="Wyoming">Wyoming</option>
+
+	</select>
+</div>
+<div class="mc-field-group input-group">
+    <strong>Gender  <span class="asterisk">*</span>
+</strong>
+    <ul><li><input type="radio" value="Male" name="GENDER" id="mce-GENDER-0"><label for="mce-GENDER-0">Male</label></li>
+<li><input type="radio" value="Female" name="GENDER" id="mce-GENDER-1"><label for="mce-GENDER-1">Female</label></li>
+</ul>
+</div>
+<div class="mc-field-group input-group">
+    <strong>Select One  <span class="asterisk">*</span>
+</strong>
+    <ul><li><input type="radio" value="Professional Fighter" name="PROORAM" id="mce-PROORAM-0"><label for="mce-PROORAM-0">Professional Fighter</label></li>
+<li><input type="radio" value="Amateur Fighter" name="PROORAM" id="mce-PROORAM-1"><label for="mce-PROORAM-1">Amateur Fighter</label></li>
+</ul>
+</div>
+<div class="mc-field-group size1of2">
+	<label for="mce-WEIGHT">Fighting Weight (lbs) Example: &quot;175&quot;  <span class="asterisk">*</span>
+</label>
+	<input type="number" name="WEIGHT" class="required" value="" id="mce-WEIGHT">
+</div>
+<div class="mc-field-group">
+	<label for="mce-RECORD">Win/Loss Record (example: &quot;5/1&quot;)  <span class="asterisk">*</span>
+</label>
+	<input type="text" value="" name="RECORD" class="required" id="mce-RECORD">
+</div>
+<div class="mc-field-group">
+	<label for="mce-ADDITIONAL">Additional Comments </label>
+	<input type="text" value="" name="ADDITIONAL" class="" id="mce-ADDITIONAL">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_9fee9de85873d2979b7968c45_d2fee3952d" value=""></div>
+	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+</div>
+<script type="text/javascript">
+var fnames = new Array();var ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[5]='PHONE';ftypes[5]='phone';fnames[4]='STATE';ftypes[4]='dropdown';fnames[9]='GENDER';ftypes[9]='radio';fnames[3]='PROORAM';ftypes[3]='radio';fnames[6]='WEIGHT';ftypes[6]='number';fnames[7]='RECORD';ftypes[7]='text';fnames[8]='ADDITIONAL';ftypes[8]='text';
+try {
+    var jqueryLoaded=jQuery;
+    jqueryLoaded=true;
+} catch(err) {
+    var jqueryLoaded=false;
+}
+var head= document.getElementsByTagName('head')[0];
+if (!jqueryLoaded) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js';
+    head.appendChild(script);
+    if (script.readyState && script.onload!==null){
+        script.onreadystatechange= function () {
+              if (this.readyState == 'complete') mce_preload_check();
+        }    
+    }
+}
+
+var err_style = '';
+try{
+    err_style = mc_custom_error_style;
+} catch(e){
+    err_style = '#mc_embed_signup input.mce_inline_error{border-color:#6B0505;} #mc_embed_signup div.mce_inline_error{margin: 0 0 1em 0; padding: 5px 10px; background-color:#6B0505; font-weight: bold; z-index: 1; color:#fff;}';
+}
+var head= document.getElementsByTagName('head')[0];
+var style= document.createElement('style');
+style.type= 'text/css';
+if (style.styleSheet) {
+  style.styleSheet.cssText = err_style;
+} else {
+  style.appendChild(document.createTextNode(err_style));
+}
+head.appendChild(style);
+setTimeout('mce_preload_check();', 250);
+
+var mce_preload_checks = 0;
+function mce_preload_check(){
+    if (mce_preload_checks>40) return;
+    mce_preload_checks++;
+    try {
+        var jqueryLoaded=jQuery;
+    } catch(err) {
+        setTimeout('mce_preload_check();', 250);
+        return;
+    }
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'http://downloads.mailchimp.com/js/jquery.form-n-validate.js';
+    head.appendChild(script);
+    try {
+        var validatorLoaded=jQuery("#fake-form").validate({});
+    } catch(err) {
+        setTimeout('mce_preload_check();', 250);
+        return;
+    }
+    mce_init_form();
+}
+function mce_init_form(){
+    jQuery(document).ready( function($) {
+      var options = { errorClass: 'mce_inline_error', errorElement: 'div', onkeyup: function(){}, onfocusout:function(){}, onblur:function(){}  };
+      var mce_validator = $("#mc-embedded-subscribe-form").validate(options);
+      $("#mc-embedded-subscribe-form").unbind('submit');//remove the validator so we can get into beforeSubmit on the ajaxform, which then calls the validator
+      options = { url: 'http://victoriummma.us3.list-manage.com/subscribe/post-json?u=9fee9de85873d2979b7968c45&id=d2fee3952d&c=?', type: 'GET', dataType: 'json', contentType: "application/json; charset=utf-8",
+                    beforeSubmit: function(){
+                        $('#mce_tmp_error_msg').remove();
+                        $('.datefield','#mc_embed_signup').each(
+                            function(){
+                                var txt = 'filled';
+                                var fields = new Array();
+                                var i = 0;
+                                $(':text', this).each(
+                                    function(){
+                                        fields[i] = this;
+                                        i++;
+                                    });
+                                $(':hidden', this).each(
+                                    function(){
+                                        var bday = false;
+                                        if (fields.length == 2){
+                                            bday = true;
+                                            fields[2] = {'value':1970};//trick birthdays into having years
+                                        }
+                                    	if ( fields[0].value=='MM' && fields[1].value=='DD' && (fields[2].value=='YYYY' || (bday && fields[2].value==1970) ) ){
+                                    		this.value = '';
+									    } else if ( fields[0].value=='' && fields[1].value=='' && (fields[2].value=='' || (bday && fields[2].value==1970) ) ){
+                                    		this.value = '';
+									    } else {
+									        if (/\[day\]/.test(fields[0].name)){
+    	                                        this.value = fields[1].value+'/'+fields[0].value+'/'+fields[2].value;									        
+									        } else {
+    	                                        this.value = fields[0].value+'/'+fields[1].value+'/'+fields[2].value;
+	                                        }
+	                                    }
+                                    });
+                            });
+                        $('.phonefield-us','#mc_embed_signup').each(
+                            function(){
+                                var fields = new Array();
+                                var i = 0;
+                                $(':text', this).each(
+                                    function(){
+                                        fields[i] = this;
+                                        i++;
+                                    });
+                                $(':hidden', this).each(
+                                    function(){
+                                        if ( fields[0].value.length != 3 || fields[1].value.length!=3 || fields[2].value.length!=4 ){
+                                    		this.value = '';
+									    } else {
+									        this.value = 'filled';
+	                                    }
+                                    });
+                            });
+                        return mce_validator.form();
+                    }, 
+                    success: mce_success_cb
+                };
+      $('#mc-embedded-subscribe-form').ajaxForm(options);
+      
+      
+    });
+}
+function mce_success_cb(resp){
+    $('#mce-success-response').hide();
+    $('#mce-error-response').hide();
+    if (resp.result=="success"){
+        $('#mce-'+resp.result+'-response').show();
+        $('#mce-'+resp.result+'-response').html(resp.msg);
+        $('#mc-embedded-subscribe-form').each(function(){
+            this.reset();
+    	});
+    } else {
+        var index = -1;
+        var msg;
+        try {
+            var parts = resp.msg.split(' - ',2);
+            if (parts[1]==undefined){
+                msg = resp.msg;
+            } else {
+                i = parseInt(parts[0]);
+                if (i.toString() == parts[0]){
+                    index = parts[0];
+                    msg = parts[1];
+                } else {
+                    index = -1;
+                    msg = resp.msg;
+                }
+            }
+        } catch(e){
+            index = -1;
+            msg = resp.msg;
+        }
+        try{
+            if (index== -1){
+                $('#mce-'+resp.result+'-response').show();
+                $('#mce-'+resp.result+'-response').html(msg);            
+            } else {
+                err_id = 'mce_tmp_error_msg';
+                html = '<div id="'+err_id+'" style="'+err_style+'"> '+msg+'</div>';
+                
+                var input_id = '#mc_embed_signup';
+                var f = $(input_id);
+                if (ftypes[index]=='address'){
+                    input_id = '#mce-'+fnames[index]+'-addr1';
+                    f = $(input_id).parent().parent().get(0);
+                } else if (ftypes[index]=='date'){
+                    input_id = '#mce-'+fnames[index]+'-month';
+                    f = $(input_id).parent().parent().get(0);
+                } else {
+                    input_id = '#mce-'+fnames[index];
+                    f = $().parent(input_id).get(0);
+                }
+                if (f){
+                    $(f).append(html);
+                    $(input_id).focus();
+                } else {
+                    $('#mce-'+resp.result+'-response').show();
+                    $('#mce-'+resp.result+'-response').html(msg);
+                }
+            }
+        } catch(e){
+            $('#mce-'+resp.result+'-response').show();
+            $('#mce-'+resp.result+'-response').html(msg);
+        }
+    }
+}
+
+</script>
+<!--End mc_embed_signup-->
+
+                                            <div id="clear"></div>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                </div>
+                                
+                               <?php include 'rightsidebar.php'; ?>
+                            
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <?php include 'footer.php'; ?>
+                    
+                </div>
+                
+            </div>
+            
+        </div>
+    
+    </body>
+
+</html>    
