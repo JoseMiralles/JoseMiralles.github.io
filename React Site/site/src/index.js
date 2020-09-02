b8dbe28e6d9ca00011f0fff485a72b146df6cf8e
Jose <magicomiralles@gmail.com>
2020-09-02 17:51:01 -0400

    Added Sass and Bootstrap

--- React Site/site/src/index.js
+++ React Site/site/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import './main.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
