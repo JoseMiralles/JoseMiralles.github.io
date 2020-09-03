b80bbb22ef739c86e5eb28e2427b639669e8392b
Jose <magicomiralles@gmail.com>
2020-09-03 16:06:46 -0400

    Scene finally renders!
    Had to add "src" attribute directly to the a-entity, instead of adding it to "<a-asset-item/>" element first.
    However, this would be a problem if I need to create multiple instances of the same asset.

--- React Site/site/src/Components/AFrameScene.js
+++ React Site/site/src/Components/AFrameScene.js
@@ -6,6 +6,8 @@ class AFrameScene extends React.Component {
     const sceneMain3DAsset = require("./Cyberpunk Scene/AFrame_CyberP_Scene/Cyberpunk_Scene.glb");
     const giantScreenImage = require("./Cyberpunk Scene/if-kipling.png");
     const bigScreenVideo = require("./Cyberpunk Scene/AFrame_CyberP_Scene/LargemonitorVideo.mp4");
+    
+
 
     return (
 
@@ -14,34 +16,33 @@ class AFrameScene extends React.Component {
         <a-scene loading-screen="backgroundColor: none" embedded background="color: #715874" renderer="foveationLevel: 2; highRefreshRate: true; colorManagement: true">
           {/* Asset Declarations */}
           <a-assets>
-            <a-asset-item id="main_scene" src={sceneMain3DAsset} />
             {/* Video Assets */}
-            <video className="Remove_from_mobile_VR" id="LargemonitorVideo" autoPlay="true" loop="true" type="video/mp4" src={bigScreenVideo} webkit-playsinline playsInline preload="auto" autoload="true" />
-            {/* <video class="Remove_from_mobile_VR" id="SmallMonitorVideo" autoplay="true" loop="true" type="video/mp4"
+            <video className="Remove_from_mobile_VR" id="LargemonitorVideo" autoPlay={true} loop={true} type="video/mp4" src={bigScreenVideo} webkit-playsinline="true" playsInline preload="auto" autoload="true" />
+            {/* <video class="Remove_from_mobile_VR" id="SmallMonitorVideo" autoplay={true} loop={true} type="video/mp4"
           src="Cyberpunk Scene\AFrame_CyberP_Scene\LargemonitorVideo.mp4" webkit-playsinline playsinline preload="auto" autoload="true"></video> */}
-            <img id="kipling-if-poem-image" src={giantScreenImage} />
+            <img id="kipling-if-poem-image" src={giantScreenImage} alt="Welcome to my site!" />
           </a-assets>
           {/* Standard ligthing */}
           <a-entity id="main_light" light="type: ambient; color: #a78fff; intensity: 1" position="-1 3.115 0.139" />
           {/* Disabled on mobile */}
           <a-entity className="Remove_from_mobile_VR" light="type: point; color: #ffffff; intensity: 1" position="1.210 3.115 0.139" />
           {/* 3D object Instances */}
-          <a-entity id="main_entity" gltf-model="#main_scene" material="shader:flat;" position="0 0 0" scale="0.9 0.9 0.9" />
+          <a-entity id="main_entity" gltf-model={sceneMain3DAsset} material="shader:flat;" position="0 0 0" scale="0.9 0.9 0.9" />
           {/* Monitor Videos */}
           {/* Large Monitor */}
-          <a-video className="LargeMonitorVideo Remove_from_mobile_VR" src="#LargemonitorVideo" position="-1.552 1.157 -1.430" rotation="0 45.950 0" scale="0.910 0.910 0.00001" width="1.45" height="0.7" play="true" />
+          <a-video className="LargeMonitorVideo Remove_from_mobile_VR" src={"#LargemonitorVideo"} position="-1.552 1.157 -1.430" rotation="0 45.950 0" scale="0.910 0.910 0.00001" width="1.45" height="0.7" play={true} />
           {/* Small Monitors, right side (Ordered from right to left.)
       <a-video class="SmallMonitorVideo Remove_from_mobile_VR" src="#SmallMonitorVideo" position="-0.109 1.005 -1.774"
-        rotation="0 1.130 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play="true"></a-video>
+        rotation="0 1.130 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play={true}></a-video>
       <a-video class="SmallMonitorVideo Remove_from_mobile_VR" src="#SmallMonitorVideo" position="-0.398 1.479 -1.814"
-        rotation="0 -12.210 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play="true"></a-video>
+        rotation="0 -12.210 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play={true}></a-video>
       <a-video class="SmallMonitorVideo Remove_from_mobile_VR" src="#SmallMonitorVideo" position="-0.587 1.005 -1.774"
-        rotation="0 1.130 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play="true"></a-video>
+        rotation="0 1.130 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play={true}></a-video>
       <!-- Small Monitors, Left side (Ordered from top to Bottom.)
       <a-video class="SmallMonitorVideo Remove_from_mobile_VR" src="#SmallMonitorVideo" position="-1.929 1.005 -0.526"
-        rotation="0 91.160 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play="true"></a-video>
+        rotation="0 91.160 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play={true}></a-video>
       <a-video class="SmallMonitorVideo Remove_from_mobile_VR" src="#SmallMonitorVideo" position="-1.999 1.466 -0.545"
-        rotation="0 104.480 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play="true"></a-video>
+        rotation="0 104.480 0" scale="0.900 0.900 0.00001" width="0.45" height="0.45" play={true}></a-video>
          */}
           {/* Giant screen poem */}
           <a-image src="#kipling-if-poem-image" position="2.456 1.630 0.130" rotation="0 -90 0" scale="4.050 2.270 1">
