899470c5bbbe77734e54a1141f3ef853822d7998
Jose Miralles <josemiralles1991@gmail.com>
2020-09-04 16:37:36 -0400

    Updates

---
+++ precache-manifest.72eb919339cb2a79078e6d3cc8ecc8e1.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e007de9042cb00a9a0540b7f1141c98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2396a32c8809263dc060",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "486581aaa3bc108053c8",
+    "url": "/static/css/main.e2f3fb5e.chunk.css"
+  },
+  {
+    "revision": "2396a32c8809263dc060",
+    "url": "/static/js/2.8778f188.chunk.js"
+  },
+  {
+    "revision": "b048f3508d9f118a50bca53c60efa000",
+    "url": "/static/js/2.8778f188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "486581aaa3bc108053c8",
+    "url": "/static/js/main.8c1257f2.chunk.js"
+  },
+  {
+    "revision": "16d50208a04408d1bf24",
+    "url": "/static/js/runtime-main.23041e79.js"
+  },
+  {
+    "revision": "21bb70b62317f276f2e97a919ff5bd8c",
+    "url": "/static/media/BebasNeue-Regular.21bb70b6.ttf"
+  },
+  {
+    "revision": "3001cff3181eefd8db327cd724309a09",
+    "url": "/static/media/Caveat-Bold.3001cff3.ttf"
+  },
+  {
+    "revision": "f9eea5c844b63860b371497b25dc639c",
+    "url": "/static/media/Cyberpunk_Scene.f9eea5c8.glb"
+  },
+  {
+    "revision": "f71515ee8d97fd37c2d4943c50043222",
+    "url": "/static/media/Eurolamy_logo.f71515ee.png"
+  },
+  {
+    "revision": "fd0ed435ec08aae4c87867bf3068762d",
+    "url": "/static/media/JosefinSans-Italic-VariableFont_wght.fd0ed435.ttf"
+  },
+  {
+    "revision": "e9583f70781e4207ccbcb0e733349f83",
+    "url": "/static/media/LargemonitorVideo.e9583f70.mp4"
+  },
+  {
+    "revision": "08f5e6a8aa826c4b0a5623f9eade2337",
+    "url": "/static/media/Maxx_isometric-ish_downscaled.08f5e6a8.png"
+  },
+  {
+    "revision": "c863f8028c2505f92540e0ba7c379002",
+    "url": "/static/media/PermanentMarker-Regular.c863f802.ttf"
+  },
+  {
+    "revision": "967db2ee867bfae77b61e7d4c09588c8",
+    "url": "/static/media/TAC-Logo.967db2ee.png"
+  },
+  {
+    "revision": "960cf57e5631aba3e732eb7c3131a018",
+    "url": "/static/media/blender_logo.960cf57e.png"
+  },
+  {
+    "revision": "466b21ae91c773f692f7686bd625ed20",
+    "url": "/static/media/cpplogo.466b21ae.png"
+  },
+  {
+    "revision": "f732ce4441b6a6d460afec6b4da115d1",
+    "url": "/static/media/gifaroo_icon.f732ce44.png"
+  },
+  {
+    "revision": "6a8b58b5b78871ae89a71a490054ff7b",
+    "url": "/static/media/if-kipling.6a8b58b5.png"
+  },
+  {
+    "revision": "9dc2bcd19c6db1f7e2c820ffde27f04f",
+    "url": "/static/media/unreal_logo.9dc2bcd1.png"
+  }
+]);
