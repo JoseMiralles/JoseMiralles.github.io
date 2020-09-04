e943ec1bf157da84dbfeffad1db695ec5ad5e7bf
Jose <magicomiralles@gmail.com>
2020-09-03 23:11:30 -0400

    React site is almost done.
    Static data is now loaded from separate file.

--- React Site/site/src/Components/ResumeSection.js
+++ React Site/site/src/Components/ResumeSection.js
@@ -4,24 +4,41 @@ import ResumeItem from "./ResumeItem.js";
 
 class ResumeSection extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.props = props;
     }
 
     render() {
 
-        const workItem = this.props.content;
+        // Map resume items if this section contains items.
+        let resumeItems = null;
+        if (this.props.SectionData.items) {
+            resumeItems = this.props.SectionData.items.map((item) => {
+                return (
+                    <ResumeItem content={item} />
+                );
+            });
+        }
+
+        // Surround by div row if this section is for smaller items.
+        if (resumeItems && this.props.SectionData.smallItems){
+            resumeItems = (
+                <div className="row my-5">
+                    {resumeItems}
+                </div>
+            );
+        }
 
         return (
 
-            <div className="page_section dark_section">
+            <div className={this.props.SectionData.darkTheme ? "page_section dark_section" : "page_section light_section"}>
                 <div className="section_title_wrapper">
-                    <h1 className="section_title">{this.props.content.title}</h1>
+                    <h1 className="section_title">{this.props.SectionData.title}</h1>
                 </div>
                 <div className="container">
 
-                    <ResumeItem content={workItem.items[0]} />
+                    {resumeItems ? resumeItems : ""}
 
                 </div>
                 <div className="container"><hr /></div>
