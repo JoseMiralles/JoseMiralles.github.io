f89e965f0e93d794b8b555d8c73bec42c7f343e4
Jose <josemiralles1991@gmail.com>
2020-09-10 10:55:47 -0400

    Modified built with react component

--- src/Components/BuiltWithReact.js
+++ src/Components/BuiltWithReact.js
@@ -5,13 +5,18 @@ class BuiltWithReact extends React.Component {
         return (
 
             <div class="row m-4">
-                <div class="col-lg-6 offset-lg-3 built_with_react_component border rounded">
-                    <div class=" p-3 d-flex justify-content-center">
-                        This site was built with React.
-                    <a class="pl-2" href="https://github.com/JoseMiralles/JoseMiralles.github.io">
-                            View source &gt;&gt;&gt;
-                    </a>
+                <div class="col-lg-6 offset-lg-3 p-3 built_with_react_component border rounded">
+
+                    <div class="row justify-content-center">
+                        Built with React.
                     </div>
+
+                    <div class="row justify-content-center">
+                        <a href="https://github.com/JoseMiralles/JoseMiralles.github.io">
+                            View source &gt;&gt;&gt;
+                        </a>
+                    </div>
+
                 </div>
             </div>
 
