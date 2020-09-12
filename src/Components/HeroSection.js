19bf7a5b48c3a294b2f54393cb73a0e6d33addd3
Jose <josemiralles1991@gmail.com>
2020-09-12 15:08:35 -0400

    added more color to the top with extra svgs.

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -5,17 +5,33 @@ class HeroSection extends React.Component {
         return (
             <div className="container-fluid light-font hero-section d-flex flex-column">
 
-                <svg class="top-svg" viewBox="0 0 100 100" preserveAspectRatio="none">
-                    <polygon points="0,0 0,70 100,100 100,20 75,0">
-                        <animate dur="1s" attributeName="points"
-                            from="0,0 0,60 100,100 100,20 65,0"
-                            to="0,0 0,70 100,100 100,20 75,0" />
-                    </polygon>
-                </svg>
+                <span class="top-svg-group">
+                    <svg class="top-svg _1" viewBox="0 0 100 100" preserveAspectRatio="none">
+                        <polygon points="0,0 0,55 100,100 100,20 60,0">
+                            <animate dur="1s" attributeName="points"
+                                from="0,0 0,50 100,100 100,20 80,0"
+                                to="0,0 0,55 100,100 100,20 60,0" />
+                        </polygon>
+                    </svg>
+                    <svg class="top-svg _2" viewBox="0 0 100 100" preserveAspectRatio="none">
+                        <polygon points="0,0 0,60 100,100 100,20 50,0">
+                            <animate dur="1s" attributeName="points"
+                                from="0,0 0,55 100,100 100,20 70,0"
+                                to="0,0 0,60 100,100 100,20 50,0" />
+                        </polygon>
+                    </svg>
+                    <svg class="top-svg _3" viewBox="0 0 100 100" preserveAspectRatio="none">
+                        <polygon points="0,0 0,70 100,100 100,20 40,0">
+                            <animate dur="1s" attributeName="points"
+                                from="0,0 0,60 100,100 100,20 60,0"
+                                to="0,0 0,70 100,100 100,20 40,0" />
+                        </polygon>
+                    </svg>
+                </span>
 
                 <div className="top-section-arrow">
                     <svg viewBox="0 0 100 100" preserveAspectRatio="none">
-                        <polyline class="st0" points="0,0 49.5,100 100,0 "/>
+                        <polyline class="st0" points="0,0 49.5,100 100,0 " />
                     </svg>
                 </div>
 
