4b6aad8a891c5a1182d6c92106cfe4993aaf7380
Jose Miralles <josemiralles1991@gmail.com>
2022-05-21 10:23:22 -0400

    removed old site files, and created new site

---
+++ frontend/src/HeroSection.tsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+import "./HeroSection.scss";
+
+const HeroSection = () => {
+
+    return (
+        <div id="hero-section" className="center column">
+
+            <h1 id="hero-title">JOSE MIRALLES</h1>
+
+            <div id="hero-jobtitle">Systems Engineer</div>
+            
+            <div id="hero-company">Bank of America</div>
+
+        </div>
+    );
+};
+
+export default HeroSection;
